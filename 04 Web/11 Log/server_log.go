26a1b6893f6e1f141bf90c48dce0a562ccef949f
witchhunter <anil.kulkarni@gmail.com>
2018-06-21 11:09:27 +0530

    added server log

--- 04 Web/11 Log/server_log.go
+++ 04 Web/11 Log/server_log.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"./middleware"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
@@ -18,15 +19,20 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, Gopher!")
 }
 
+func startPanic(w http.ResponseWriter, r *http.Request) {
+	panic("Starting a panic...")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", index).Methods("GET")
 	r.HandleFunc("/hello", hello).Methods("GET")
+	r.HandleFunc("/panic", startPanic).Methods("GET")
 
 	// Logging middleware
 	// Gorilla handler logs out a standard apache format log
 	logger := handlers.LoggingHandler(os.Stdout, r)
 
 	log.Println("Starting server at http://localhost:8080/")
-	log.Fatalln(http.ListenAndServe(":8080", logger))
+	log.Fatalln(http.ListenAndServe(":8080", middleware.PanicRecovery(logger)))
 }
